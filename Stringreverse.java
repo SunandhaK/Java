a0874f3d90e452a98eb093503c6ee48e4f045a0c
SunandhaK <chinnik11294@gmail.com>
2021-04-20 17:43:16 +0530

    First commit

---
+++ Stringreverse.java
@@ -0,0 +1,14 @@
+package Java1;
+
+public class Stringreverse {
+
+	public static void main(String[] args)
+	{
+	String str = "Sunandha";
+	//System.out.println(str.length());
+	StringBuffer sb = new StringBuffer(str);
+	System.out.println(sb.reverse());
+	
+	}
+
+}
